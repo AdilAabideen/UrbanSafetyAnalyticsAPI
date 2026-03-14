f29d463a6117418354e8db4f1f17daa160e62a79
Adil Aabideen <aaabideen08@gmail.com>
2026-03-14 03:13:11 +0000

    removed Useless Endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,11 +5,7 @@ from fastapi.responses import JSONResponse
 
 from .api.analytics import router as analytics_router
 from .api.auth import router as auth_router
-from .api.collisions import router as collisions_router
-from .api.crimes import router as crimes_router
-from .api.lsoa import router as lsoa_router
 from .api.report_events import router as report_events_router
-from .api.roads import router as roads_router
 from .api.tiles import router as tiles_router
 from .api.watchlist import router as watchlist_router
 from .bootstrap import initialize_database
@@ -23,13 +19,10 @@ from .errors import (
 
 
 app = FastAPI(title="Urban Risk Analytics API")
-app.include_router(roads_router)
-app.include_router(crimes_router)
-app.include_router(collisions_router)
+
 app.include_router(tiles_router)
 app.include_router(analytics_router)
 app.include_router(auth_router)
-app.include_router(lsoa_router)
 app.include_router(report_events_router)
 app.include_router(watchlist_router)
 

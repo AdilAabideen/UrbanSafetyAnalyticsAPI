fd01fd8e4eaf966d59a6dcde40b9b24e142adb85
Adil Aabideen <aaabideen08@gmail.com>
2026-03-15 21:46:25 +0000

    Added Road Service _ WIll Delete Later

---
+++ frontend/src/services/roadService.js
@@ -0,0 +1,42 @@
+import { API_BASE_URL } from "../config/env";
+import { fetchJson, toFeatureCollection, toQueryString } from "./serviceUtils";
+
+export const roadsService = {
+  getVectorTilesUrl({ startMonth, endMonth, crime = true, collisions = true, userReportedEvents = true } = {}) {
+    const params = new URLSearchParams();
+    if (startMonth) params.set("startMonth", startMonth);
+    if (endMonth) params.set("endMonth", endMonth);
+    params.set("crime", String(Boolean(crime)));
+    params.set("collisions", String(Boolean(collisions)));
+    params.set("userReportedEvents", String(Boolean(userReportedEvents)));
+    return `${API_BASE_URL}/tiles/roads/{z}/{x}/{y}.mvt?${params}`;
+  },
+
+  async getRoadsInBoundingBox(
+    { minLon, minLat, maxLon, maxLat, limit = 1200 },
+    requestOptions = {},
+  ) {
+    const query = toQueryString({ minLon, minLat, maxLon, maxLat, limit });
+    const data = await fetchJson(
+      `${API_BASE_URL}/roads?${query}`,
+      "Failed to fetch roads in viewport",
+      requestOptions,
+    );
+
+    return toFeatureCollection(data);
+  },
+
+  async getNearestRoad({ lon, lat }, requestOptions = {}) {
+    const query = toQueryString({ lon, lat });
+    return fetchJson(`${API_BASE_URL}/roads/nearest?${query}`, "Failed to fetch nearest road", requestOptions);
+  },
+
+  async getRoadStats({ minLon, minLat, maxLon, maxLat }, requestOptions = {}) {
+    const query = toQueryString({ minLon, minLat, maxLon, maxLat });
+    return fetchJson(`${API_BASE_URL}/roads/stats?${query}`, "Failed to fetch road stats", requestOptions);
+  },
+
+  async getRoadById(id, requestOptions = {}) {
+    return fetchJson(`${API_BASE_URL}/roads/${id}`, "Failed to fetch road details", requestOptions);
+  },
+};
